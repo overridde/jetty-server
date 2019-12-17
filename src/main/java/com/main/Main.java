a79e1dd2a26a2db737737c5572a8d21520e59d52
Aleksey <myEmail@example.com>
2019-12-17 22:58:11 +0300

    Third commit

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -4,6 +4,7 @@ import org.eclipse.jetty.server.Server;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.out.println("Start Server");
         Server server = new Server(8080);
 
         server.start();
