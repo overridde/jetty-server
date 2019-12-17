5e82d8bff98c34384e06598f75d862dd3015bb44
Aleksey <myEmail@example.com>
2019-12-17 21:43:43 +0300

    First commit

---
+++ src/main/java/com/main/Main.java
@@ -0,0 +1,11 @@
+package com.main;
+
+import org.eclipse.jetty.server.Server;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        server.start();
+    }
+}
